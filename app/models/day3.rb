87bc693077d9219822b151f5b7805a9482e8b077
Carlos Gochez <carlos.gochez@checkr.com>
2023-12-03 10:28:06 -0700

    Day3 part 2

--- app/models/day3.rb
+++ app/models/day3.rb
@@ -7,11 +7,14 @@ class Day3
 
   def self.go_test
     day3 = Day3.new(true)
-    puts day3.sum_parts
+    puts "Sum parts: #{day3.sum_parts}"
+    puts "Ratio: #{day3.sum_ratio_gears}"
   end
 
   def self.go
-    puts Day3.new.sum_parts
+    day3 = Day3.new
+    puts "Sum parts: #{day3.sum_parts}"
+    puts "Ratio: #{day3.sum_ratio_gears}"
   end
 
   def sum_parts
@@ -36,6 +39,47 @@ class Day3
           end
           line = line[(num_index+num.length)..(line.length - 1)]
           removed += num_index + num.length
+        end
+      end
+    end
+    sum
+  end
+
+  def sum_ratio_gears
+    sum = 0
+    @puzzle.each_with_index do |line, row_index|
+      # Extract all the *
+      asterisks = line.split(/[^\*]/)
+      asterisks.delete("")
+
+      if asterisks
+        # there is numbers! Check each
+        removed = 0
+        asterisks.each do |ast|
+          num_index = line.index(ast)
+          num_left = number(row_index, num_index + removed - 1)
+          num_right = number(row_index, num_index + removed + 1)
+          num_above = number(row_index - 1, num_index + removed)
+          num_down = number(row_index + 1, num_index + removed)
+          num_d_up_left = nil
+          num_d_up_right = nil
+          unless num_above
+            num_d_up_left = number(row_index - 1, num_index + removed - 1)
+            num_d_up_right = number(row_index - 1, num_index + removed + 1)
+          end
+          num_d_down_left = nil
+          num_d_down_right = nil
+          unless num_down
+            num_d_down_left = number(row_index + 1, num_index + removed - 1)
+            num_d_down_right = number(row_index + 1, num_index + removed + 1)
+          end
+          nums = [num_left, num_right, num_above, num_down, num_d_up_left, num_d_up_right, num_d_down_left, num_d_down_right].compact
+          puts "Nums found #{nums}"
+          if nums.length == 2
+            sum += nums.map{|n| n.to_i}.inject(:*)
+          end
+          line = line[(num_index+1)..(line.length - 1)]
+          removed += num_index + 1
         end
       end
     end
@@ -70,5 +114,27 @@ class Day3
 
   def symbol?(element)
     element.present? && element != '.' && !element.match?(/[[:digit:]]/)
+  end
+
+  def number(row_index, pos)
+    return nil if row_index < 0
+    return nil if row_index == @puzzle.length
+
+    line = @puzzle[row_index]
+    return nil if pos < 0
+    return nil if pos == line.length
+    return nil unless line[pos].match?(/[[:digit:]]/)
+
+    init = pos
+    while line[init].present? && line[init].match?(/[[:digit:]]/)
+      init -= 1
+    end
+    stop = pos
+    while line[stop].present? && line[stop].match?(/[[:digit:]]/)
+      stop += 1
+    end
+    num = line[(init+1)..(stop-1)]
+    puts "Num found: #{num}"
+    num
   end
 end
